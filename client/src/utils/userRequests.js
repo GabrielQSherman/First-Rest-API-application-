7774f262452dcdd591bcb1de226f94c04c442583
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-17 23:07:03 -0400

    fix error in username FE validation

--- client/src/utils/userRequests.js
+++ client/src/utils/userRequests.js
@@ -1,5 +1,5 @@
 const {default: axios} = require('axios');
-const {default: val} = require('validator')
+const {default: validate} = require('validator')
 const baseURL = 'http://localhost:4000';
 const isEmpty = require('./isEmpty');
 
@@ -12,14 +12,37 @@ module.exports = {
       const val = input.value.trim();
       if (!isEmpty(val)) {
         reqBody[input.name] = val
+        switch (input.name) {
+          case 'credential':
+              if ( !validate.isEmail(val) ) {
+                const username = reqBody.credential;
+                if ( !validate.isAscii(username) ) failedValues.push({ key: 'username', error: 'Username Using Invalid Characters'})
+                if ( username.length < 3 ) failedValues.push({ key: 'username', error: 'Username Needs To Be At Least 3 Characters'})
+                if ( !username.length > 21 ) failedValues.push({ key: 'username', error: 'Username Needs To Be Shorter Than 21 Characters'})
+              } else {
+                const email = reqBody.email;
+                if ( !validate.isEmail(email)) failedValues.push({key: 'email', error: 'Email Invalid'})
+                if ( email.length < 7 ) failedValues.push({key: 'email', error: 'Email Needs To Be At Least 7 Characters'})
+                if ( email.length > 255 ) failedValues.push({key: 'email', error: 'Email Needs To Be Shorter Than 256 Characters'})
+                if ( !validate.isAscii(email)) failedValues.push({key: 'email', error: 'Email Is Using Invalid Characters'})      
+              }
+            break;
+          case 'password':
+            const password = reqBody.password;
+                if ( password.length < 7 ) failedValues.push({key: 'password', error: 'Password Needs To Be At Least 7 Characters'})
+                if ( password.length > 1000 ) failedValues.push({key: 'password', error: 'Password Needs To Be Shorter Than 1000 Characters'})
+            break;         
+        }
       } else {
+
         failedValues.push({key: input.name, error: `${input.name} Is Required`})
+
       }
     }
-
-    // [] lengths of inputs (match schema)
-    if (!isEmpty(reqBody.email) && !val.isEmail(reqBody.email)) failedValues.push({key: 'email', error: 'Email Invalid'})
-
+    
+    if (failedValues.length !== 0) {
+      return alert(failedValues.map(err=>{return `Error with ${err.key}:\n${err.error}\n`}).join('\n'))
+    }
 
     const loginURL = `${baseURL}/user/login`; 
 
