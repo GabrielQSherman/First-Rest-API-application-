045fcce062393680ecbe20b5788ff669723e24e0
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-18 23:38:15 -0400

    create component that wil set the theme state and link to other pages

---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Button from './Button';
+
+import useTheme from '../Hooks/useTheme';
+
+const loginLink = 'login';
+const regLink = 'register';
+
+export default function NavBar() {
+  
+  const [dm, toggleDm] = useTheme(true);
+  
+  return (
+    <div
+      style={{
+        width: '100%',
+        margin: 0,
+        paddingTop: 30,
+        paddingBottom: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: dm ? '#222' : 'pink',
+        justifyContent: 'space-between'
+      }}
+      >
+      <Button
+        onClick= {() => {window.location = window.location.origin}}
+        text='Home'
+        style={{marginLeft: 30}}
+      />
+      <Button
+        onClick= {() => {window.location = regLink}}
+        text='Create An Account'
+        style={{color: 'white', backgroundColor: 'green'}}
+      />
+      <Button
+        onClick= {() => {window.location = loginLink}}
+        text='Login To Your Account'
+      />
+      <Button 
+        onClick= {() => {toggleDm( prvDm => {return !prvDm})}}
+        text= { dm ? 'Turn On Light Mode' : 'Turn On Dark Mode'}
+        style= {{marginRight: 30}}
+      />
+
+    </div>
+  )
+}
