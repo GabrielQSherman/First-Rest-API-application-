0398a7ba02cf7575dc2358c82db249dbf3faadc1
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-18 23:42:24 -0400

    implement custom theme hook into Button

--- client/src/Components/Button.js
+++ client/src/Components/Button.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
+import useTheme from '../Hooks/useTheme';
+
 export default function Button(props) {
+  
+  const [dm] = useTheme(false);
+
+  const buttonStyle = {
+    padding: 10,
+    borderRadius: 5,
+    fontSize: 'large',
+    backgroundColor: dm ? '#333' : 'lightblue',
+    color: dm ? 'white' : 'deeppink',
+    cursor: 'pointer',
+  }
 
   return (
     <button
@@ -12,13 +25,4 @@ export default function Button(props) {
       {props.text}
     </button>
   )
-}
-
-const buttonStyle = {
-  padding: 10,
-  borderRadius: 5,
-  fontSize: 'large',
-  backgroundColor: 'lightblue',
-  color: 'hotpink',
-  cursor: 'pointer',
 }
