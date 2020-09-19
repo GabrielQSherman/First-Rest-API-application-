38227eb792d79e1829296b2ec424a511a0e9f949
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-19 09:29:35 -0400

    create a context and hook to provide the theme to all child componenet in main app

---
+++ client/src/Hooks/ThemeContext.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useContext, createContext} from 'react';
+
+const { get, set} = require('../utils/localStorage');
+
+const ThemeContext = createContext()
+
+const ThemeUpdateContext = createContext()
+
+export function useTheme() {
+  return useContext(ThemeContext)
+}
+
+export function useThemeUpdate() {
+  return useContext(ThemeUpdateContext)
+}
+
+export function ThemeProvider({ children }) {
+  const [dm, setTheme] = useState( () => {
+    return get('theme', false)    
+  })
+
+  useEffect(() => {
+
+    set('theme', JSON.stringify(dm))
+    document.getElementById('root').style.backgroundColor = dm ? '#333': 'white';
+  }, [dm])
+
+  const toggleDM = () => {
+    setTheme(prvDm => !prvDm)
+  } 
+
+  return (
+    <ThemeContext.Provider value={dm}>
+        <ThemeUpdateContext.Provider value={toggleDM}>
+          {children}
+        </ThemeUpdateContext.Provider>
+    </ThemeContext.Provider>
+  )
+}
