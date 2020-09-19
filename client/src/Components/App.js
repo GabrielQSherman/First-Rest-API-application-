f928ee61122d7795584e205e3fb936cf20e87dfe
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-19 17:44:36 -0400

    implementing hook into App component

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import { BrowserRouter } from 'react-router-dom'; 
 
@@ -6,7 +6,12 @@ import AppRouter from './AppRouter';
 
 import { ThemeProvider } from '../Hooks/ThemeContext'
 
+import useCountVists from '../Hooks/useCountVisits';
+
 function App() {
+
+  const pv = useCountVists(0)
+
   return (
     <ThemeProvider >
       <BrowserRouter>
--- client/src/Hooks/useCountVisits.js
+++ client/src/Hooks/useCountVisits.js
@@ -3,9 +3,13 @@ import {useState, useEffect} from 'react'
 const { get, set} = require('../utils/localStorage');
 
 export default function useCountVisits(initVal) {
+
+  console.log(initVal);
+
   const [visits, setVisits] = useState( () => {
     return get('visits', initVal)    
   })
+
 
   useEffect(() => {
 
