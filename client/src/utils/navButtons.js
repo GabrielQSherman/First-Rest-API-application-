15bedfc8af8da37db4d3f1bbc3499f129de0e18e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-21 12:51:55 -0400

    create file for navbuts that will be created dynamically in navBar

---
+++ client/src/utils/navButtons.js
@@ -0,0 +1,6 @@
+
+export default [
+  {location: `${window.location.origin}/`, text: 'Home'},
+  {location: `${window.location.origin}/register`, text: 'Create An Account', style: {color: 'white', backgroundColor: 'green'}},
+  {location: `${window.location.origin}/login`, text: 'Login To Your Account'},
+]
