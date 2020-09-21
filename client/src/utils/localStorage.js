05fe25d25a06edef7f8e38d782d82845b32b3c02
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-20 23:36:56 -0400

    update get method to deal with case where localStorage value starts null

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -2,7 +2,7 @@ module.exports = {
   get: (key, initVal) => {
     const saved = JSON.parse(localStorage.getItem(key));
 
-    if (saved !== undefined) return saved
+    if (saved !== undefined && saved !== null ) return saved
     
     if (initVal instanceof Function) return initVal()
     return initVal
