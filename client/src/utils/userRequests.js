bf7b864f418cecdd0b15f25bc6d5d6cfca79c4d6
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-30 22:24:06 -0400

    changing request to be more dynamic and scalable with new forms and requests

--- client/src/utils/userRequests.js
+++ client/src/utils/userRequests.js
@@ -1,119 +1,213 @@
-const {default: axios} = require('axios');
 const {default: validate} = require('validator')
 const baseURL = 'http://localhost:4000';
 const isEmpty = require('./isEmpty');
 
 module.exports = {
-  loginReq: async (values) => {
-    const 
-      reqBody = {},
-      failedValues = [];
-    for (const key in values) {
-      const val = values[key].trim();
-      if (!isEmpty(val)) {
-        reqBody[key] = val
-        switch (key) {
-          case 'credential':
-              if ( !validate.isEmail(val) ) {
-                const username = reqBody.credential;
-                if ( !validate.isAscii(username) ) failedValues.push({ key: 'username', error: 'Username Using Invalid Characters'})
-                if ( username.length < 3 ) failedValues.push({ key: 'username', error: 'Username Needs To Be At Least 3 Characters'})
-                if ( !username.length > 21 ) failedValues.push({ key: 'username', error: 'Username Needs To Be Shorter Than 21 Characters'})
-              } else {
-                const email = reqBody.email;
-                if ( !validate.isEmail(email)) failedValues.push({key: 'email', error: 'Email Invalid'})
-                if ( email.length < 7 ) failedValues.push({key: 'email', error: 'Email Needs To Be At Least 7 Characters'})
-                if ( email.length > 255 ) failedValues.push({key: 'email', error: 'Email Needs To Be Shorter Than 256 Characters'})
-                if ( !validate.isAscii(email)) failedValues.push({key: 'email', error: 'Email Is Using Invalid Characters'})      
-              }
-            break;
-          case 'password':
-            const password = reqBody.password;
-                if ( password.length < 7 ) failedValues.push({key: 'password', error: 'Password Needs To Be At Least 7 Characters'})
-                if ( password.length > 1000 ) failedValues.push({key: 'password', error: 'Password Needs To Be Shorter Than 1000 Characters'})
-            break;  
-          default:       
+  login: {
+    endpoint: baseURL+'/login',
+    method: 'PUT',
+    validation: (data) => {
+          const failedValues = [];
+          for (const key in data) {
+          const val = data[key].trim();
+          if (!isEmpty(val)) {
+            switch (key) {
+              case 'credential':
+                  if ( !validate.isEmail(val) ) {
+                    const username = val;
+                    if ( !validate.isAscii(username) ) failedValues.push({ key: 'credential', error: 'Username Using Invalid Characters'})
+                    if ( username.length < 3 ) failedValues.push({ key: 'credential', error: 'Username Needs To Be At Least 3 Characters'})
+                    if ( !username.length > 21 ) failedValues.push({ key: 'credential', error: 'Username Needs To Be Shorter Than 21 Characters'})
+                  } else {
+                    const email = val;
+                    if ( !validate.isEmail(email)) failedValues.push({key: 'credential', error: 'Email Invalid'})
+                    if ( email.length < 7 ) failedValues.push({key: 'credential', error: 'Email Needs To Be At Least 7 Characters'})
+                    if ( email.length > 255 ) failedValues.push({key: 'credential', error: 'Email Needs To Be Shorter Than 256 Characters'})
+                    if ( !validate.isAscii(email)) failedValues.push({key: 'credential', error: 'Email Is Using Invalid Characters'})      
+                  }
+                break;
+              case 'password':
+                const password = val;
+                    if ( password.length < 7 ) failedValues.push({key: 'password', error: 'Password Needs To Be At Least 7 Characters'})
+                    if ( password.length > 1000 ) failedValues.push({key: 'password', error: 'Password Needs To Be Shorter Than 1000 Characters'})
+                break;  
+              default:       
+            }
+          } else {
+            failedValues.push({key: key, error: `${key} Is Required`})
+          }
         }
-      } else {
-
-        failedValues.push({key: key, error: `${key} Is Required`})
-
-      }
+        return failedValues
     }
-    
-    if (failedValues.length !== 0) {
-      return alert(failedValues.map(err=>{return `Error with ${err.key}:\n${err.error}\n`}).join('\n'))
-    }
-
-    const loginURL = `${baseURL}/user/login`; 
-
-    return fetch(loginURL, {
-        method: "PUT",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(reqBody)
-    })
-    .then(rs => { 
-      console.log(rs);
-      
-      if (rs.status === 200) {
-        alert("You've successully logged in")
-      }
-
-      return rs.json()
-    })
-    .then(res => {
-      console.log(res)
-      
-    })
-
-    // return await axios.put(loginURL, reqBody).then(response => response.data)
-    
   },
-
-  regReq: async (values) => {
-
-    const reqBody = {};
-
-    for (const key in values) {
-      
-      const val = values[key].trim();
-
-      if (val !== '') {
-        reqBody[key] = val
-      }
-    
+  register: {
+    endpoint: baseURL+'/register',
+    method: 'POST ',
+    validation: (data) => {
+          const failedValues = [];
+          for (const key in data) {
+          const val = data[key].trim();
+          if (!isEmpty(val)) {
+            switch (key) {
+              case 'email':
+                const email = val;
+                  if ( !validate.isEmail(email)) failedValues.push({key: 'email', error: 'Email Invalid'})
+                  if ( email.length < 7 ) failedValues.push({key: 'email', error: 'Email Needs To Be At Least 7 Characters'})
+                  if ( email.length > 255 ) failedValues.push({key: 'email', error: 'Email Needs To Be Shorter Than 256 Characters'})
+                  if ( !validate.isAscii(email)) failedValues.push({key: 'email', error: 'Email Is Using Invalid Characters'})        
+                break;
+              case 'username':
+                  const username = val;
+                  if ( !validate.isAscii(username) ) failedValues.push({ key: 'username', error: 'Username Using Invalid Characters'})
+                  if ( username.length < 3 ) failedValues.push({ key: 'username', error: 'Username Needs To Be At Least 3 Characters'})
+                  if ( !username.length > 21 ) failedValues.push({ key: 'username', error: 'Username Needs To Be Shorter Than 21 Characters'})                
+                break;
+              case 'password':
+                const password = val;
+                  if ( password.length < 7 ) failedValues.push({key: 'password', error: 'Password Needs To Be At Least 7 Characters'})
+                  if ( password.length > 1000 ) failedValues.push({key: 'password', error: 'Password Needs To Be Shorter Than 1000 Characters'})
+                break;  
+              default:       
+            }
+          } else {
+            failedValues.push({key: key, error: `${key} Is Required`})
+          }
+        }
+        return failedValues
     }
-
-    //frontend validation needed:
-    // [*] make sure all feilds are present,
-    if (Object.keys(reqBody).length !== 4) {
-      return alert('You forgot to fill something out')
-    } 
-    // [] lengths of inputs (match schema)
-    //all validation from before, 
-    // [*] plus make sure both password inputs match
-    if (reqBody.password !== reqBody.password2) {
-      return alert('Passwords do not match')
-    }
-
-    const regURL = `${baseURL}/user/register`; 
-
-    const user = await axios.post(regURL, reqBody)
-    .then( res => {
-      console.log(res);
-    })
-    .catch( err => {
-      if (err) {
-        console.log(err); 
-      }
-    })
-
-    setTimeout(() => {
-
-      return user
-    }, 1000)
   }
 }
+
+// import React, {useContext} from 'react'
+// module.exports = {
+//   loginReq: async (values) => {
+
+//     const 
+//       reqBody = {},
+//       failedValues = [];
+//     for (const key in values) {
+//       const val = values[key].trim();
+//       if (!isEmpty(val)) {
+//         reqBody[key] = val
+//         switch (key) {
+//           case 'credential':
+//               if ( !validate.isEmail(val) ) {
+//                 const username = reqBody.credential;
+//                 if ( !validate.isAscii(username) ) failedValues.push({ key: 'username', error: 'Username Using Invalid Characters'})
+//                 if ( username.length < 3 ) failedValues.push({ key: 'username', error: 'Username Needs To Be At Least 3 Characters'})
+//                 if ( !username.length > 21 ) failedValues.push({ key: 'username', error: 'Username Needs To Be Shorter Than 21 Characters'})
+//               } else {
+//                 const email = reqBody.email;
+//                 if ( !validate.isEmail(email)) failedValues.push({key: 'email', error: 'Email Invalid'})
+//                 if ( email.length < 7 ) failedValues.push({key: 'email', error: 'Email Needs To Be At Least 7 Characters'})
+//                 if ( email.length > 255 ) failedValues.push({key: 'email', error: 'Email Needs To Be Shorter Than 256 Characters'})
+//                 if ( !validate.isAscii(email)) failedValues.push({key: 'email', error: 'Email Is Using Invalid Characters'})      
+//               }
+//             break;
+//           case 'password':
+//             const password = reqBody.password;
+//                 if ( password.length < 7 ) failedValues.push({key: 'password', error: 'Password Needs To Be At Least 7 Characters'})
+//                 if ( password.length > 1000 ) failedValues.push({key: 'password', error: 'Password Needs To Be Shorter Than 1000 Characters'})
+//             break;  
+//           default:       
+//         }
+//       } else {
+
+//         failedValues.push({key: key, error: `${key} Is Required`})
+
+//       }
+//     }
+    
+//     if (failedValues.length !== 0) {
+//       return alert(failedValues.map(err=>{return `Error with ${err.key}:\n${err.error}\n`}).join('\n'))
+//     }
+
+//     const loginURL = `${baseURL}/user/login`; 
+
+//     const xhr = new XMLHttpRequest();
+
+//     xhr.open('PUT', loginURL, false);
+//     xhr.setRequestHeader('Content-Type', 'application/json')
+
+//     xhr.onload = () => {
+//       const res = JSON.parse(xhr.responseText)
+//       console.log(res);
+//     }
+
+//     xhr.send(JSON.stringify(reqBody))
+
+//     // return fetch(loginURL, {
+//     //     method: "PUT",
+//     //     mode: "cors",
+//     //     headers: {
+//     //         "Content-Type": "application/json"
+//     //     },
+//     //     body: JSON.stringify(reqBody)
+//     // })
+//     // .then(rs => { 
+//     //   console.log(rs);
+      
+//     //   if (rs.status === 200) {
+//     //     alert("You've successully logged in")
+//     //   }
+
+//     //   return rs.json()
+//     // })
+//     // .then(res => {
+//     //   console.log(res)
+      
+//     // })
+
+//     // return axios.put(loginURL, reqBody).then(response => {
+      
+      
+//     //   return response.data
+    
+//     // })
+    
+//   },
+
+//   regReq: async (values) => {
+
+//     const reqBody = {};
+
+//     for (const key in values) {
+      
+//       const val = values[key].trim();
+
+//       if (val !== '') {
+//         reqBody[key] = val
+//       }
+    
+//     }
+
+//     //frontend validation needed:
+//     // [*] make sure all feilds are present,
+//     if (Object.keys(reqBody).length !== 4) {
+//       return alert('You forgot to fill something out')
+//     } 
+//     // [] lengths of inputs (match schema)
+//     //all validation from before, 
+//     // [*] plus make sure both password inputs match
+//     if (reqBody.password !== reqBody.password2) {
+//       return alert('Passwords do not match')
+//     }
+
+//     const regURL = `${baseURL}/user/register`; 
+
+//     const user = await axios.post(regURL, reqBody)
+//     .then( res => {
+//       console.log(res);
+//     })
+//     .catch( err => {
+//       if (err) {
+//         console.log(err); 
+//       }
+//     })
+
+//     setTimeout(() => {
+
+//       return user
+//     }, 1000)
+//   }
+// }
